97413e6d2645ae22a5c653e5aeb5074d8133c417
maple614 <highmaple614@gmail.com>
2019-07-09 18:10:40 +0900

    first commit

---
+++ Assets/Scripts/Camera_Control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Control : MonoBehaviour
+{
+    public GameObject[] children_cameras;
+    public GameObject[] targets;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("b"))
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.SetActive(false);
+            transform.GetChild(0).gameObject.GetComponent<Cinemachine.CinemachineVirtualCamera>().Follow = targets[1].transform;
+        }
+        else if(Input.GetKeyDown("a"))
+        {
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
